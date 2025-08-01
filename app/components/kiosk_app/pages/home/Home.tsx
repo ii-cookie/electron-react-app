c3f9d4357402b1a08c9db8dc805baa4d4e1b1c3d
ii-cookie <isaacnleong@gmail.com>
2025-08-01 10:38:18 +0800

    added slider, yet to modify

--- app/components/kiosk_app/pages/home/Home.tsx
+++ app/components/kiosk_app/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import './Home.css'
 import FeatureList from './components/FeatureList'
+import PosterSlider from './components/PosterSlider'
 
 const Home = () => {
   return (
     <>
+      <PosterSlider />
       <FeatureList />
     </>
   )
--- app/components/kiosk_app/pages/home/components/PosterSlider.tsx
+++ app/components/kiosk_app/pages/home/components/PosterSlider.tsx
@@ -1,15 +1,65 @@
 import './PosterSlider.css'
 import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import image1 from './1.jpg'
+import image2 from './2.jpg'
+import image3 from './3.jpg'
+import image4 from './4.jpg'
+
+const data = [
+  {
+    image: image1,
+    title: '1',
+  },
+  {
+    image: image2,
+    title: '2',
+  },
+  {
+    image: image3,
+    title: '3',
+  },
+  {
+    image: image4,
+    title: '4',
+  },
+]
 
 const PosterSlider = () => {
-  var settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   }
-  return <Slider {...settings}>{/*insert a list of posters here*/}</Slider>
+  return (
+    <div className="PosterSlider w-3/4 m-auto ">
+      <Slider {...settings}>
+        {data.map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt={item.title} className="w-60 h-60 object-cover" />
+            </div>
+            <div className="p-5 bg-amber-300">
+              <p className="font-semibold">{item.title}</p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      {/* <Slider {...settings}>
+        <div></div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+      </Slider> */}
+    </div>
+  )
 }
 
 export default PosterSlider
