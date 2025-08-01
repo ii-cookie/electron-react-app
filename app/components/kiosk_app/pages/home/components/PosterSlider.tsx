13f6104fa0ddff10a097283fb3fe0b96143e4ce4
ii-cookie <isaacnleong@gmail.com>
2025-08-01 17:22:17 +0800

    updated correct dimension of poster

--- app/components/kiosk_app/pages/home/components/PosterSlider.tsx
+++ app/components/kiosk_app/pages/home/components/PosterSlider.tsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-//NOTE THAT IMAGE MUST BE >= 900x1550 TO FILL WHOLE POSTER
+//NOTE THAT IMAGE MUST BE >= 810x1550 TO FILL WHOLE POSTER
 
 import image1 from './1.jpg'
 import image2 from './2.jpg'
