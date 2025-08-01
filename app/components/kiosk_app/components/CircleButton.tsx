506230f893a173e7ca6044acdbaece93f506f3e5
ii-cookie <isaacnleong@gmail.com>
2025-08-01 16:46:31 +0800

    made the home circle button reusable

---
+++ app/components/kiosk_app/components/CircleButton.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import { Button } from '../../ui/button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { ReactElement } from 'react'
+
+interface Prop {
+  icon: IconDefinition
+  name: string
+  url: string
+}
+
+const CircleButton = ({ icon, name, url }: Prop) => {
+  return (
+    <div className="CircleButton">
+      <Link to={url}>
+        <Button
+          variant="default"
+          className="rounded-full w-[100px] h-[100px] text-lg flex flex-col items-center justify-center gap-1"
+        >
+          <FontAwesomeIcon icon={icon} className="text-4xl" />
+          <span>{name}</span>
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+export default CircleButton
--- app/components/kiosk_app/components/HomeButton.tsx
+++ app/components/kiosk_app/components/HomeButton.tsx
@@ -1,20 +1,11 @@
-import { Link } from 'react-router-dom'
-import { Button } from '../../ui/button'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
+import CircleButton from './CircleButton'
 
 const HomeButton = () => {
   return (
     <div className="HomeButton">
-      <Link to="/">
-        <Button
-          variant="default"
-          className="rounded-full w-[100px] h-[100px] text-lg flex flex-col items-center justify-center gap-1"
-        >
-          <FontAwesomeIcon icon={faHouse} className="text-4xl" />
-          <span>Home</span>
-        </Button>
-      </Link>
+      <CircleButton icon={faHouse} name="Home" url="/"></CircleButton>
     </div>
   )
 }
