1edffd9a6d650bbc1f55ecceef555926f3d4b3ac
ii-cookie <isaacnleong@gmail.com>
2025-08-08 15:45:19 +0800

    changed folder to layout

--- app/components/kiosk_app/components/Layout/CircleButton.tsx
+++ app/components/kiosk_app/components/Layout/CircleButton.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Button } from '../../ui/button'
+import { Button } from '../../../ui/button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
 
