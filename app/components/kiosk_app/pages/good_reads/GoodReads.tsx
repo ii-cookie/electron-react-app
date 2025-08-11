f60dae22bfff7319da15c1d3dd8f5fdd88746842
ii-cookie <isaacnleong@gmail.com>
2025-08-11 15:21:09 +0800

    made simple book card and grid to contain books

--- app/components/kiosk_app/pages/good_reads/GoodReads.tsx
+++ app/components/kiosk_app/pages/good_reads/GoodReads.tsx
@@ -1,11 +1,23 @@
 import BFAButton from '../../components/Layout/BFAButton'
 import HomeButton from '../../components/Layout/HomeButton'
 import BookCard from './book/BookCard'
+import img1 from './images/61HkdyBpKOL._UF1000,1000_QL80_.jpg'
 
 const GoodReads = () => {
   return (
     <div className="GoodReads">
-      <BookCard />
+      <div className="bookshelf grid grid-cols-3">
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+        <BookCard imageSrc={img1} title="1984" author="George Orwell" />
+      </div>
       <HomeButton /> <BFAButton />
     </div>
   )
--- app/components/kiosk_app/pages/good_reads/book/BookCard.tsx
+++ app/components/kiosk_app/pages/good_reads/book/BookCard.tsx
@@ -1,5 +1,19 @@
-const BookCard = () => {
-  return <div>BookCard</div>
+import './BookCard.css'
+
+const BookCard = ({ imageSrc, title, author }) => {
+  return (
+    <div className="book-card">
+      <div className="book-cover-container">
+        <img src={imageSrc} alt={title} className="book-cover" />
+      </div>
+
+      <div className="book-details">
+        <h3 className="book-title">{title}</h3>
+        <p className="book-author">{author}</p>
+        <button className="fake-button">E-Reading/Borrowing</button>
+      </div>
+    </div>
+  )
 }
 
 export default BookCard
